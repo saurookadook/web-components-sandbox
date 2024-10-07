a9e354549ba3e6974ff3df51453fb49cc32caeca
Andy Maskiell <maskiella@gmail.com>
2024-10-06 20:59:19 -0400

    Fix eslint and prettier settings

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+const plugins = ['prettier-plugin-multiline-arrays'];
+
+module.exports = {
+    plugins: plugins,
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSameLine: false,
+    arrowParens: 'always',
+    proseWrap: 'always',
+    multilineArraysWrapThreshold: 2,
+};
---
+++ eslint.config.js
@@ -0,0 +1,102 @@
+const env = {
+    es6: true,
+    jest: true,
+    node: true,
+    browser: true,
+};
+
+const _extends = [
+    'prettier/prettier',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
+];
+
+const _globals = {
+    expect: false,
+    assert: false,
+    __DEVTOOLS__: false,
+    window: true,
+};
+
+const _plugins = [
+    'prettier',
+    'jsx-a11y',
+];
+
+const _rules = {
+    'array-bracket-newline': [1, 'consistent'],
+    'array-bracket-spacing': [
+        1,
+        'never',
+        {
+            arraysInArrays: true,
+            objectsInArrays: false,
+        },
+    ],
+    'array-element-newline': [
+        0,
+        {
+            multiline: true,
+            minItems: 2,
+        },
+    ],
+    'arrow-parens': [2, 'always'],
+    curly: [1, 'multi-line'],
+    'class-methods-use-this': 0,
+    'keyword-spacing': [
+        2,
+        {
+            before: true,
+            after: true,
+            overrides: {
+                function: {
+                    after: false,
+                },
+                while: {
+                    after: false,
+                },
+            },
+        },
+    ],
+    'jsx-quotes': [2, 'prefer-double'],
+    'max-len': 0,
+    'no-extraneous-imports': 0,
+    'no-inner-declarations': 0,
+    'no-prototype-builtins': 0,
+    'no-unused-expressions': 0,
+    'no-unused-vars': 1,
+    'no-use-before-define': 0,
+    'no-useless-escape': 0,
+    'object-curly-spacing': [1, 'always'],
+    'operator-linebreak': 0,
+    'prettier/prettier': 0,
+    semi: [1, 'always'],
+    'space-before-function-paren': 0,
+    'wrap-iife': [2, 'inside'],
+};
+
+module.exports = [
+    {
+        // "root": true,
+        files: ['*.js'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: {
+                __DEVTOOLS__: _globals.__DEVTOOLS__,
+                window: _globals.window,
+            },
+            // parser: espree.parse() || espree.parseForESLint(),
+            /**
+             * @description The type of JavaScript source code. Possible values are "script"
+             *  for traditional script files, "module" for ECMAScript modules (ESM), and
+             *  "commonjs" for CommonJS files. (default: "module" for .js and .mjs files;
+             *  "commonjs" for .cjs files)
+             */
+            sourceType: 'module',
+        },
+        // plugins: _plugins,
+        rules: _rules,
+        // settings: _settings
+    },
+];
