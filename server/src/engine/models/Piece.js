49454045cd280ea93587a0a59002f42932b66ef0
Andy Maskiell <maskiella@gmail.com>
2024-10-11 12:00:27 -0400

    Create basic Piece model

---
+++ server/src/engine/models/Piece.js
@@ -0,0 +1,55 @@
+import { PLAYER_COLORS, PIECE_TYPES, CHESS_PIECES } from '../../constants/pieces';
+
+class Piece {
+    constructor({ color, type }) {
+        if (this._areValidOptions({ color, type })) {
+            this._color = color;
+            this._type = type;
+        }
+    }
+
+    get unicode() {
+        return CHESS_PIECES.get(this._color).get(this._type);
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    _areValidOptions({ color, type }) {
+        let areValid = false;
+        let exception = null;
+
+        try {
+            areValid = this._isValidColor(color) && this._isValidType(type);
+        } catch (e) {
+            exception = e;
+        }
+
+        if (!areValid) {
+            throw exception || new TypeError('Color and/or type is invalid!');
+        }
+
+        return areValid;
+    }
+
+    _isValidColor(color) {
+        if (!PLAYER_COLORS.includes(color)) {
+            throw TypeError(`Type '${typeof color}' for argument ${color} for 'color' is invalid!`);
+        }
+        return true;
+    }
+
+    _isValidType(type) {
+        if (!PIECE_TYPES.includes(type)) {
+            throw TypeError(`Type '${typeof type}' for argument ${type} for 'color' is invalid!`);
+        }
+        return true;
+    }
+}
+
+export default Piece;
